2e28790a382fe65d1cd714de0e4ceb09e55f457f
Jakub Próchnicki <jakub.prochnicki.stud@pw.edu.pl>
2024-04-02 13:41:17 +0200

    Wykrycie i eliminacja reszty wycieków pamięci

--- src/Solution.c
+++ src/Solution.c
@@ -1,48 +1,94 @@
-#include<stdlib.h>
-#include<stdio.h>
-#include "MazeSolution.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include "Solution.h"
 
-void printSolution(Stack *stack, FILE *plik)
+int initStack(Stack **stack)
 {
-	rewind(plik);
-	while(stack->top!=NULL && stack->top->next!=NULL)
-	{
-
-		int* cords=readCords(plik, stack->top->node);
-		int x_curr=cords[0];
-		int y_curr=cords[1];
-		cords=readCords(plik, stack->top->next->node);
-		int x_next=cords[0];
-		int y_next=cords[1];
-
-		if(x_curr==x_next)
-		{
-			if(y_curr>y_next)
-			{
-				printf("Góra %d\n", y_curr-y_next);
-			}
-			if(y_curr<y_next)
-			{
-				printf("Dół %d\n", y_next-y_curr);
-			}
-		}
-
-		else if(y_curr==y_next)
-		{
-			if(x_curr>x_next)
-			{
-				printf("Lewo %d\n", x_curr-x_next);
-			}
-			if(x_curr<x_next)
-			{
-				printf("Prawo %d\n", x_next-x_curr);
-			}
-		}
-
-		else
-			printf("Błąd\n");
-		stack->top=stack->top->next;
-	}
+        *stack=malloc(sizeof(Stack));
+        if(*stack==NULL)
+        {
+                printf("Nie udalo sie zainicjowac stosu\n");
+                return 1;
+        }
+        (*stack)->top=NULL;
+        return 0;
 }
 
+int push(Stack *stack, int node)
+{
+    Node *newNode=malloc(sizeof(Node));
+    if(newNode==NULL)
+    {
+        printf("Nie udalo sie zaalokowac pamieci na wezel\n");
+        return 1;
+    }
+    newNode->node=node;
+    newNode->next=stack->top;
+    stack->top=newNode;
+
+    return 0;
+}
+
+int pop(Stack *stack)
+{
+    if(stack==NULL || stack->top==NULL)
+    {
+        printf("Stos jest pusty\n");
+        return 1;
+    }
+
+    Node *temp=stack->top;
+    stack->top=temp->next;
+
+    free(temp);
+    return 0;
+}
+
+void reverse(Stack *stack)
+{
+        if(stack->top!=NULL)
+        {
+                int temp=stack->top->node;
+                pop(stack);
+                reverse(stack);
+
+                insertAtBottom(stack, temp);
+        }
+}
+
+void insertAtBottom(Stack *stack, int node)
+{
+        if(stack->top==NULL)
+                push(stack, node);
+        else
+        {
+                int temp = stack->top->node;
+                pop(stack);
+                insertAtBottom(stack, node);
+
+                push(stack, temp);
+
+        }
+}
+
+void printStack(Stack *stack)
+{
+        Node *temp=stack->top;
+        while(temp!=NULL)
+        {
+                printf("%d\n", temp->node);
+                temp=temp->next;
+        }
+}
+
+void freeStack(Stack *stack)
+{
+        while(stack->top!=NULL)
+        {
+                Node *temp=stack->top;
+                stack->top=stack->top->next;
+                free(temp);
+        }
+	free(stack);
+}
 
--- src/Solution.h
+++ src/Solution.h
@@ -1,8 +1,25 @@
-#ifndef MAZESOLUTION_H
-#define MAZESOLUTION_H
-#include "Stack.h"
-#include "MazeReader.h"
-void printSolution(Stack *stack, FILE *plik);
+#ifndef MAZE_SOLUTION
+#define MAZE_SOLUTION
 
+typedef struct Node
+{
+    int node;
+    struct Node *next;
+}Node;
+
+typedef struct Stack
+{
+        Node* top;
+}Stack;
+
+Node *createNode(int node);
+int initStack(Stack **stack);
+int isEmpty(Stack *stack);
+int push(Stack *stack, int node);
+int pop(Stack *stack);
+void reverse(Stack *stack);
+void insertAtBottom(Stack *stack, int node);
+void printStack(Stack *stack);
+void freeStack(Stack *stack);
 
 #endif
