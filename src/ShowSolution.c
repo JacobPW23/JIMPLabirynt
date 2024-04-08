bddedcdb79178c30f53b54ca2593d70c6bf2b7dc
Michał <michal.zalewski11.stud@pw.edu.pl>
2024-04-09 00:14:08 +0200

    Uproszczenie funkcji rozwiązującej, obsługa przypadku, gdy trzy kolejne wierzchołki leżą na prostej

--- src/ShowSolution.c
+++ src/ShowSolution.c
@@ -1,3 +1,5 @@
+#include <string.h>
+#include <stdlib.h>
 #include "Graph.h"
 #include "Solution.h"
 #include "ShowSolution.h"
@@ -5,22 +7,48 @@
 void printSolution(Path *stack, Graph graph)
 {
     PathNode *temp=stack->top;
+    int prev_x=graph->coords[2*(temp->node)];
+    int prev_y=graph->coords[2*(temp->node)+1];
+    int dist=0;
+    char direction[10]="";
     while(temp!=NULL && temp->next!=NULL)
     {
-        if(graph->coords[2*(temp->node)] == graph->coords[2*(temp->next->node)])
+	int curr_x=graph->coords[2*(temp->node)];
+	int curr_y=graph->coords[2*(temp->node)+1];
+	int next_x=graph->coords[2*(temp->next->node)];
+	int next_y=graph->coords[2*(temp->next->node)+1];
+	
+        if(curr_x == next_x)
         {
-            if(graph->coords[2*(temp->node)+1] > graph->coords[2*(temp->next->node)+1])
-                printf("Góra %d \n", graph->coords[2*(temp->node)+1]-graph->coords[2*(temp->next->node)+1]);
-            else if(graph->coords[2*(temp->node)+1] < graph->coords[2*(temp->next->node)+1])
-                printf("Dół %d \n", graph->coords[2*(temp->next->node)+1]-graph->coords[2*(temp->node)+1]);
+	    int diff=curr_y-next_y;
+            if(dist==0 || strcmp(direction, diff > 0 ? "góra" : "dół")==0)
+	    {
+		    strcpy(direction, diff > 0 ? "góra" : "dół");
+		    dist+=abs(diff);
+	    }
+            else
+	    { 
+                printf("%s %d\n", direction, dist);
+		strcpy(direction, diff > 0 ? "góra" : "dół");
+		dist=abs(diff);
+	    }
+	    	
         }
 
-        else if(graph->coords[2*(temp->node)+1] == graph->coords[2*(temp->next->node)+1])
+        else if(curr_y == next_y)
         {
-            if(graph->coords[2*(temp->node)] > graph->coords[2*(temp->next->node)])
-                printf("Lewo %d \n", graph->coords[2*(temp->node)]-graph->coords[2*(temp->next->node)]);
-            else if(graph->coords[2*(temp->node)] < graph->coords[2*(temp->next->node)])
-                printf("Prawo %d \n", graph->coords[2*(temp->next->node)]-graph->coords[2*(temp->node)]);
+	    int diff=curr_x-next_x;
+            if(dist==0 || strcmp(direction, diff > 0 ? "lewo" : "prawo")==0)
+	    {
+		    strcpy(direction, diff > 0 ? "lewo" : "prawo");
+		    dist+=abs(diff);
+	    }
+            else
+	    {
+		    printf("%s %d\n", direction, dist);
+		    strcpy(direction, diff > 0 ? "lewo" : "prawo");
+		    dist=abs(diff);
+	    }
         }
         else
             printf("Błąd\n");
